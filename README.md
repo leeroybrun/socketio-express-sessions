1b44230dcf089c9f9cd1e6d84b3507bedddbfd4a
Leeroy Brun <leeroy.brun@gmail.com>
2013-10-23 11:01:59 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-socketio-express-sessions
-=========================
+# Socket.IO + Express 3 sessions
 
-Demonstrate how to use Express 3 sessions in Socket.IO for authentification.
+The goal of this project is to demonstrate how to use Express 3 sessions in Socket.IO for authentification.
+
+To achieve this we will store the Express sessions in a MemoryStore. We can then simply get the session ID from the client's cookies and extract the session from the MemoryStore.
+
+There are already some examples out there on how to do that, you can found links at the end of this page.
+My goal here was just to give a complete example and not to need to ´require('connect')´ directly in our project, as Express has it own cookie parser.
+
+## Login process
+
+We have a small login process, here the user just need to access the ´/login´ page to be logged in. He can then be logged out by accessing the ´/logout´ page.
+In real world apps the login process will be a bit more complex, but hey, it's just a demo ;-).
+
+The login route will just set a ´loggedIn´ session flag to ´true´.
+
+## Socket.IO handshake
+
+The Socket.IO authorization handler is used to accept or reject connections to the socket server.
+
+In fact, it's a simple HTTP call made from the client to the server. So we can get Express cookies from this call and parse them.
+In these cookies we can then find the Express SID, which will be used to extract the session data from the MemoryStore.
+
+We then just have to check if the ´loggedIn´ flag is set to ´true´ and accept the connection.
+If it's not, or if the session doesn't exist, we refuse the connection to the socket server.
+
+## Initial credits / ideas
+- http://www.danielbaulig.de/socket-ioexpress/
+- https://github.com/wcamarao/session.socket.io
+- http://notjustburritos.tumblr.com/post/22682186189/socket-io-and-express-3
+- http://howtonode.org/socket-io-auth
---
+++ server.js
@@ -0,0 +1,99 @@
+var io      =   require('socket.io'),
+	http    =   require('http'),
+	express =   require('express');
+
+var PORT = process.env.PORT || 8080,
+	HOST = process.env.HOST || 'localhost';
+
+// We define the key of the cookie containing the Express SID
+var EXPRESS_SID_KEY = 'express.sid';
+
+// We define a secret string used to crypt the cookies sent by Express
+var COOKIE_SECRET = 'very secret string';
+var cookieParser = express.cookieParser(COOKIE_SECRET);
+
+// Create a new store in memory for the Express sessions
+var sessionStore = new express.session.MemoryStore();
+
+var app = express();
+
+// Configure Express app with :
+// * Cookie Parser created above
+// * Configure Session Store
+app.configure(function () {
+	app.use(cookieParser);
+	app.use(express.session({
+        store: sessionStore,
+        cookie: { 
+            httpOnly: true
+        },
+        key: EXPRESS_SID_KEY
+    }));
+});
+
+// Configture routes
+app.get('/', function (req, res) {
+	res.sendfile(__dirname + '/views/index.html');
+});
+
+// Very basic login/logout routes
+app.get('/login', function (req, res) {
+	// We just set a session value indicating that the user is logged in
+	req.session.isLogged = true;
+	res.redirect('/');
+});
+app.get('/logout', function (req, res) {
+	req.session.isLogged = false;
+	res.redirect('/');
+});
+
+// Create HTTP server, register socket.io as listener
+server = http.createServer(app);
+io = io.listen(server);
+
+// We configure the socket.io authorization handler (handshake)
+io.set('authorization', function (data, callback) {
+    if(!data.headers.cookie) {
+        return callback('No cookie transmitted.', false);
+    }
+
+    // We use the Express cookieParser created before to parse the cookie
+    // Express cookieParser(req, res, next) is used initialy to parse data in "req.headers.cookie".
+    // Here our cookies are stored in "data.headers.cookie", so we just pass "data" to the first argument of function
+    cookieParser(data, {}, function(parseErr) {
+        if(parseErr) { return callback('Error parsing cookies.', false); }
+
+        // Get the SID cookie
+        var sidCookie = (data.secureCookies && data.secureCookies[EXPRESS_SID_KEY]) ||
+                        (data.signedCookies && data.signedCookies[EXPRESS_SID_KEY]) ||
+                        (data.cookies && data.cookies[EXPRESS_SID_KEY]);
+
+        // Then we just need to load the session from the Express Session Store
+        sessionStore.load(sidCookie, function(err, session) {
+        	// And last, we check if the used has a valid session and if he is logged in
+            if (err || !session || session.isLogged !== true) {
+                callback('Error', false);
+            } else {
+            	// If you want, you can attach the session to the handshake data, so you can use it again later
+                data.session = session;
+
+                callback(null, true);
+            }
+        });
+    });
+});
+
+// upon connection, start a periodic task that emits (every 1s) the current timestamp
+io.on('connection', function (socket) {
+	var sender = setInterval(function () {
+		socket.emit('myCustomEvent', new Date().getTime());
+	}, 1000);
+
+	socket.on('disconnect', function() {
+		clearInterval(sender);
+	});
+});
+
+ server.listen(PORT, HOST, null, function() {
+    console.log('Server listening on port %d in %s mode', this.address().port, app.settings.env);
+});
