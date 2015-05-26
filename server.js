cec09ac6cf38d62d3cb54d68bc2fa63f21f89fbd
Leeroy Brun <leeroy.brun@gmail.com>
2015-05-26 19:04:42 +0200

    Upgrade to Express 4 & Socket.io 1

--- server.js
+++ server.js
@@ -1,39 +1,42 @@
-var io      = require('socket.io'),
-    http    = require('http'),
-    express = require('express');
+var socketIo = require('socket.io');
+var http     = require('http');
+var express  = require('express');
+var expressCookieParser = require('cookie-parser');
+var expressSession = require('express-session');
 
 var PORT = process.env.PORT || 8080,
     HOST = process.env.HOST || 'localhost';
 
 // We define the key of the cookie containing the Express SID
-var EXPRESS_SID_KEY = 'express.sid';
+var EXPRESS_SID_KEY = 'connect.sid';
 
 // We define a secret string used to crypt the cookies sent by Express
 var COOKIE_SECRET = 'very secret string';
-var cookieParser = express.cookieParser(COOKIE_SECRET);
+var cookieParser = expressCookieParser(COOKIE_SECRET);
 
 // Create a new store in memory for the Express sessions
-var sessionStore = new express.session.MemoryStore();
+var sessionStore = new expressSession.MemoryStore();
 
 var app = express();
 
 // Configure Express app with :
 // * Cookie Parser created above
 // * Configure Session Store
-app.configure(function () {
-    app.use(cookieParser);
-    app.use(express.session({
-        store: sessionStore,
-        cookie: { 
-            httpOnly: true
-        },
-        key: EXPRESS_SID_KEY
-    }));
-});
+app.use(cookieParser);
+app.use(expressSession({
+    store: sessionStore,
+    cookie: { 
+        httpOnly: true
+    },
+    resave: false,
+    saveUninitialized: false,
+    secret: COOKIE_SECRET,
+    name: EXPRESS_SID_KEY
+}));
 
-// Configture routes
+// Configure routes
 app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 // Very basic login/logout routes
@@ -47,21 +50,28 @@ app.get('/logout', function (req, res) {
     res.redirect('/');
 });
 
-// Create HTTP server, register socket.io as listener
+// Create HTTP server
 server = http.createServer(app);
-io = io.listen(server);
 
-// We configure the socket.io authorization handler (handshake)
-io.set('authorization', function (data, callback) {
+// Create Socket.io server
+var io = socketIo({
+    // Optional Socket.io options
+});
+
+io.use(function(socket, next) {
+    var data = socket.request;
+
+    console.log(data);
+
     if(!data.headers.cookie) {
-        return callback('No cookie transmitted.', false);
+        return next(new Error('No cookie transmitted.'));
     }
 
     // We use the Express cookieParser created before to parse the cookie
     // Express cookieParser(req, res, next) is used initialy to parse data in "req.headers.cookie".
     // Here our cookies are stored in "data.headers.cookie", so we just pass "data" to the first argument of function
     cookieParser(data, {}, function(parseErr) {
-        if(parseErr) { return callback('Error parsing cookies.', false); }
+        if(parseErr) { return next(new Error('Error parsing cookies.')); }
 
         // Get the SID cookie
         var sidCookie = (data.secureCookies && data.secureCookies[EXPRESS_SID_KEY]) ||
@@ -71,18 +81,32 @@ io.set('authorization', function (data, callback) {
         // Then we just need to load the session from the Express Session Store
         sessionStore.load(sidCookie, function(err, session) {
             // And last, we check if the used has a valid session and if he is logged in
-            if (err || !session || session.isLogged !== true) {
-                callback('Not logged in.', false);
+            if (err) {
+                return next(err);
+
+            // Session is empty
+            } else if(!session) {
+                return next(new Error('Session cannot be found/loaded'));
+
+            // Check for auth here, here is a basic example
+            } else if (session.isLogged !== true) {
+                return next(new Error('User not logged in'));
+
+            // Everything is fine
             } else {
                 // If you want, you can attach the session to the handshake data, so you can use it again later
-                // You can access it later with "socket.handshake.session"
+                // You can access it later with "socket.request.session"
                 data.session = session;
+                data.sessionId = sidCookie;
 
-                callback(null, true);
+                return next();
             }
         });
     });
 });
+
+// 
+io.listen(server);
 
 // upon connection, start a periodic task that emits (every 1s) the current timestamp
 io.on('connection', function (socket) {
