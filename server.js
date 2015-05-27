9845b5b689ca8066b5fec4214f0bbb7eedfb9762
Leeroy Brun <leeroy.brun@gmail.com>
2015-05-27 10:30:52 +0200

    Working Express 4 and Socket.IO 1 example

--- server.js
+++ server.js
@@ -21,17 +21,14 @@ var app = express();
 
 // Configure Express app with :
 // * Cookie Parser created above
-// * Configure Session Store
+// * Configure session
 app.use(cookieParser);
 app.use(expressSession({
-    store: sessionStore,
-    cookie: { 
-        httpOnly: true
-    },
-    resave: false,
-    saveUninitialized: false,
-    secret: COOKIE_SECRET,
-    name: EXPRESS_SID_KEY
+    store: sessionStore,        // We use the session store created above
+    resave: false,              // Do not save back the session to the session store if it was never modified during the request
+    saveUninitialized: false,   // Do not save a session that is "uninitialized" to the store
+    secret: COOKIE_SECRET,      // Secret used to sign the session ID cookie. Must use the same as speficied to cookie parser
+    name: EXPRESS_SID_KEY       // Custom name for the SID cookie
 }));
 
 // Configure routes
@@ -43,10 +40,17 @@ app.get('/', function (req, res) {
 app.get('/login', function (req, res) {
     // We just set a session value indicating that the user is logged in
     req.session.isLogged = true;
+
+    // Just an example to show how to get session data between Express and Socket.IO
+    req.session.username = 'Hello.World';
+
     res.redirect('/');
 });
+
 app.get('/logout', function (req, res) {
     req.session.isLogged = false;
+    delete req.session.username;
+
     res.redirect('/');
 });
 
@@ -58,25 +62,26 @@ var io = socketIo({
     // Optional Socket.io options
 });
 
+// Socket.IO 1 is now using middlewares
+// We can use this functionnality to implement authentification
 io.use(function(socket, next) {
-    var data = socket.request;
+    var request = socket.request;
 
-    console.log(data);
-
-    if(!data.headers.cookie) {
+    if(!request.headers.cookie) {
+        // If we want to refuse authentification, we pass an error to the first callback
         return next(new Error('No cookie transmitted.'));
     }
 
     // We use the Express cookieParser created before to parse the cookie
     // Express cookieParser(req, res, next) is used initialy to parse data in "req.headers.cookie".
-    // Here our cookies are stored in "data.headers.cookie", so we just pass "data" to the first argument of function
-    cookieParser(data, {}, function(parseErr) {
+    // Here our cookies are stored in "request.headers.cookie", so we just pass "request" to the first argument of function
+    cookieParser(request, {}, function(parseErr) {
         if(parseErr) { return next(new Error('Error parsing cookies.')); }
 
         // Get the SID cookie
-        var sidCookie = (data.secureCookies && data.secureCookies[EXPRESS_SID_KEY]) ||
-                        (data.signedCookies && data.signedCookies[EXPRESS_SID_KEY]) ||
-                        (data.cookies && data.cookies[EXPRESS_SID_KEY]);
+        var sidCookie = (request.secureCookies && request.secureCookies[EXPRESS_SID_KEY]) ||
+                        (request.signedCookies && request.signedCookies[EXPRESS_SID_KEY]) ||
+                        (request.cookies && request.cookies[EXPRESS_SID_KEY]);
 
         // Then we just need to load the session from the Express Session Store
         sessionStore.load(sidCookie, function(err, session) {
@@ -95,9 +100,9 @@ io.use(function(socket, next) {
             // Everything is fine
             } else {
                 // If you want, you can attach the session to the handshake data, so you can use it again later
-                // You can access it later with "socket.request.session"
-                data.session = session;
-                data.sessionId = sidCookie;
+                // You can access it later with "socket.request.session" and "socket.request.sessionId"
+                request.session = session;
+                request.sessionId = sidCookie;
 
                 return next();
             }
@@ -105,15 +110,22 @@ io.use(function(socket, next) {
     });
 });
 
-// 
+// Start the socket.io server
 io.listen(server);
 
-// upon connection, start a periodic task that emits (every 1s) the current timestamp
+// Upon connection, start a periodic task that emits (every 1s) the current timestamp
 io.on('connection', function (socket) {
+
+    // Just an exemple showing how to get data from the session
+    // It's in the auth middleware we assigned the session data to "socket.request", we can then easily use it here
+    socket.emit('welcome', 'Welcome '+ socket.request.session.username +'! Here is your SID: '+ socket.request.sessionId);
+
+    // An example event, sending the current timestamp every 1000 milliseconds (1s)
     var sender = setInterval(function () {
         socket.emit('myCustomEvent', new Date().getTime());
     }, 1000);
 
+    // On disconnect, clear the interval
     socket.on('disconnect', function() {
         clearInterval(sender);
     });
